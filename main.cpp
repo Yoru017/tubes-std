b5c537842dd1162bfb9b89db84b2b68f8b33b93a
Fikri Fauzi <110084024+Yoru017@users.noreply.github.com>
2023-12-23 21:52:00 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Tubes.h"
+using namespace std;
+
+int main()
+{
+    string mulai;
+    cout<<"Ketik Apapun Untuk Memulai Program"<<endl;
+    cin>>mulai;
+    MulaiMenu();
+}
